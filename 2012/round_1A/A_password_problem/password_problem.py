2703c0fe0c247c4c30c97291a750f22b51a38215
DosDos <DosDos@localhost>
2015-04-26 19:57:41 +0200

    Add code for Problem A - Round 1A 2012

--- 2012/round_1A/A_password_problem/password_problem.py
+++ 2012/round_1A/A_password_problem/password_problem.py
@@ -3,17 +3,25 @@ Created on 02/05/2015
 
 @author: Dos
 
-Problem A.
-https://code.google.com/codejam/contest/
+Problem A. Password Problem
+https://code.google.com/codejam/contest/1645485/dashboard#s=p0
 
 
 ***Sample***
 
 Input
-WRITE_INTPUT_HERE
+3
+2 5
+0.6 0.6
+1 20
+1
+3 4
+1 0.9 0.1
 
 Output
-WRITE_OUTPUT_HERE
+Case #1: 7.000000
+Case #2: 20.000000
+Case #3: 4.500000
 
 """
 
@@ -40,20 +48,25 @@ def read_decimals(f, d=' '):
 
 def solve(case, **kwargs):
     # get problem data
-    N = kwargs['N']
-    P = kwargs['P']
-    L = kwargs['L']
+    A = kwargs['A']
+    B = kwargs['B']
+    p = kwargs['p_list']
 
-    return "Case #{}: {}\n".format(case, L)
+    best, x = B + 2.0, 1
+    for i in xrange(A):
+        x *= p[i]
+        best = min(best, (B - i) + (A - i - 1) + (B + 1) * (1 - x))
+
+    return "Case #%d: %.6f\n" % (case, best)
 
 
 INPUT_FILE_NAME = "A-sample.in"
-# INPUT_FILE_NAME = "A-small-attempt0.in"
-# INPUT_FILE_NAME = "A-large.in"
+# INPUT_FILE_NAME = "A-small-practice.in"
+# INPUT_FILE_NAME = "A-large-practice.in"
 
 OUTPUT_FILE_NAME = "A-sample.out"
-# OUTPUT_FILE_NAME = "A-small-attempt0.out"
-# OUTPUT_FILE_NAME = "A-large.out"
+# OUTPUT_FILE_NAME = "A-small-practice.out"
+# OUTPUT_FILE_NAME = "A-large-practice.out"
 
 if __name__ == '__main__':
 
@@ -66,16 +79,15 @@ if __name__ == '__main__':
     print("\nThere are %d cases to solve! :)\n" % T)
 
     # iterate on each case
-    for case in range(T):
+    for case in range(1, T+1):
         # read input args
-        line_1 = read_words(input_file)
-        w1 = int(line_1[0])
-        w2 = line_1[1]
-        line_2 = read_ints(input_file)
-        args = {'N': w1, 'P': w2, 'L': line_2}
+        line_1 = read_ints(input_file)
+        line_2 = read_decimals(input_file)
+        args = {'A': line_1[0], 'B': line_1[1], 'p_list': line_2}
 
         print("Input #{}:\n{}".format(case, args))
         out = solve(case, **args)
+        print(out)
         output_file.write(out)
 
     # close I/O files
