d9699f95c970678d2ea71b8c682e2686ecbb1c94
DosDos <DosDos@localhost>
2015-05-13 09:22:02 +0200

    Load white files for Round 1C 2015

--- 2015/round_1C/A_name/A.py
+++ 2015/round_1C/A_name/A.py
@@ -10,12 +10,19 @@ https://code.google.com/codejam/contest/4244486/dashboard
 ***Sample***
 
 Input
-WRITE_INTPUT_HERE
+3
+1 4 2
+1 7 7
+2 5 1
+
 
 Output
-WRITE_OUTPUT_HERE
+Case #1: 3
+Case #2: 7
+Case #3: 10
 
 """
+from math import log
 
 
 def read_word(f):
@@ -40,20 +47,32 @@ def read_decimals(f, d=' '):
 
 def solve(case, **kwargs):
     # get problem data
-    N = kwargs['N']
-    P = kwargs['P']
-    L = kwargs['L']
+    R = kwargs['R']
+    C = kwargs['C']
+    W = kwargs['W']
 
-    return "Case #{}: {}\n".format(case, L)
+    best = W
+
+    if W == C:
+        best = W
+    else:
+        guess1 = C / W
+        guess2 = W - 1 if C % W == 0 else W
+
+        print "guess 1: ", guess1
+        print "guess 2: ", guess2
+        best = (guess1 + guess2) * R
+
+    return "Case #{}: {}\n".format(case, best)
 
 
-INPUT_FILE_NAME = "A-sample.in"
-# INPUT_FILE_NAME = "A-small-attempt0.in"
-# INPUT_FILE_NAME = "A-large.in"
+# INPUT_FILE_NAME = "A-sample.in"
+# INPUT_FILE_NAME = "A-small-attempt3.in"
+INPUT_FILE_NAME = "A-large.in"
 
-OUTPUT_FILE_NAME = "A-sample.out"
-# OUTPUT_FILE_NAME = "A-small-attempt0.out"
-# OUTPUT_FILE_NAME = "A-large.out"
+# OUTPUT_FILE_NAME = "A-sample.out"
+# OUTPUT_FILE_NAME = "A-small-attempt3.out"
+OUTPUT_FILE_NAME = "A-large.out"
 
 if __name__ == '__main__':
 
@@ -68,11 +87,11 @@ if __name__ == '__main__':
     # iterate on each case
     for case in xrange(1, T+1):
         # read input args
-        line_1 = read_words(input_file)
-        w1 = int(line_1[0])
+        line_1 = read_ints(input_file)
+        w1 = line_1[0]
         w2 = line_1[1]
-        line_2 = read_ints(input_file)
-        args = {'N': w1, 'P': w2, 'L': line_2}
+        w3 = line_1[2]
+        args = {'R': w1, 'C': w2, 'W': w3}
 
         print("Input #{}:\n{}".format(case, args))
         out = solve(case, **args)
--- 2015/round_1C/B_name/B.py
+++ 2015/round_1C/B_name/B.py
@@ -10,10 +10,29 @@ https://code.google.com/codejam/contest/4244486/dashboard
 ***Sample***
 
 Input
-WRITE_INTPUT_HERE
+5
+7 6 6
+BANANAS
+MONKEY
+2 3 4
+AA
+AAA
+2 1 2
+AB
+B
+6 2 2
+GOOGLE
+GO
+26 11 100
+ABCDEFGHIJKLMNOPQRSTUVWXYZ
+ROSENCRANTZ
 
 Output
-WRITE_OUTPUT_HERE
+Case #1: 0.0
+Case #2: 0.0
+Case #3: 1.0
+Case #4: 0.8888889
+Case #5: 9.0
 
 """
 
@@ -38,13 +57,54 @@ def read_decimals(f, d=' '):
     return [float(x) for x in read_words(f, d)]
 
 
+def find_prefix(s):
+    p = ""
+    for i in range(len(s)):
+        if s[:i] == s[-i:]:
+            p = s[:i]
+
+    return len(p)
+
+
+print find_prefix("gogogd")
+
+
 def solve(case, **kwargs):
     # get problem data
-    N = kwargs['N']
-    P = kwargs['P']
+    K = kwargs['K']
     L = kwargs['L']
+    S = kwargs['S']
+    keyword = kwargs['keyword']
+    target = kwargs['target']
 
-    return "Case #{}: {}\n".format(case, L)
+    result = -1
+    is_possible = True
+
+    clean_keyword = {}
+    for c in keyword:
+        if c in clean_keyword:
+            clean_keyword[c] += 1
+        else:
+            clean_keyword[c] = 1
+
+    for c in target:
+        if c not in clean_keyword:
+            is_possible = False
+            break
+
+    if is_possible:
+        prob = 1
+        for c in target[find_prefix(target):]:
+
+            print c, clean_keyword[c] * (1. / K)
+            prob *= clean_keyword[c] * (1. / K)
+
+        print "prob=", prob
+
+    else:
+        result = 0.0
+
+    return "Case #{}: {}\n".format(case, result)
 
 
 INPUT_FILE_NAME = "B-sample.in"
@@ -68,11 +128,13 @@ if __name__ == '__main__':
     # iterate on each case
     for case in xrange(1, T+1):
         # read input args
-        line_1 = read_words(input_file)
-        w1 = int(line_1[0])
+        line_1 = read_ints(input_file)
+        w1 = line_1[0]
         w2 = line_1[1]
-        line_2 = read_ints(input_file)
-        args = {'N': w1, 'P': w2, 'L': line_2}
+        w3 = line_1[2]
+        line_2 = read_word(input_file)
+        line_3 = read_word(input_file)
+        args = {'K': w1, 'L': w2, 'S': w3, 'keyword': line_2, 'target': line_3}
 
         print("Input #{}:\n{}".format(case, args))
         out = solve(case, **args)
