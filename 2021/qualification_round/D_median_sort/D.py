1b29300a214f1e25062ea8d8dabdee8df471cb57
dos <dav.santucci@gmail.com>
2021-04-29 17:52:11 +0200

    Load solutions for qualification round problems C, D, E

---
+++ 2021/qualification_round/D_median_sort/D.py
@@ -0,0 +1,52 @@
+"""
+Created on 26/03/2021
+
+@author: Dos
+
+Problem D. Median Sort
+https://codingcompetitions.withgoogle.com/codejam/round/000000000043580a/00000000006d1284
+
+***Sample***
+
+Sample Interaction
+Judge
+Solution
+Constraints
+2 5 600
+Judge provides T, N, Q
+Case 1
+1 2 3
+Solution asks for the median of {x1,x2,x3}
+2
+Judge responds that the median is x2
+4 2 3
+Solution asks for the median of {x4,x2,x3}
+3
+Judge responds that the median is x3
+5 4 3
+Solution asks for the median of {x5,x4,x3}
+4
+Judge responds that the median is x4
+5 4 3 2 1
+Solution outputs the sorted list
+1
+Judge confirms that the answer is correct
+Case 2
+1 2 3
+Solution asks for the median of {x1,x2,x3}
+3
+Judge responds that the median is x3
+2 3 4
+Solution asks for the median of {x2,x3,x4}
+4
+Judge responds that the median is x4
+3 4 5
+Solution asks for the median of {x3,x4,x5}
+5
+Judge responds that the median is x5
+1 3 5 4 2
+Solution outputs the sorted list
+1
+Judge confirms that the answer is correct
+
+"""
